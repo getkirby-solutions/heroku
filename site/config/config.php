e8b9e417bface0aa56c4ab88e5e74acb8a880cfe
heyallan <allanmoreno@live.com>
2017-01-03 17:42:40 -0600

    Remove debug config option

--- site/config/config.php
+++ site/config/config.php
@@ -16,8 +16,8 @@ for more information: http://getkirby.com/license
 */
 
 c::set('license', 'put your license key here');
-// c::set('debug', true);
-c::set('panel.install', true);
+c::set('debug', true);
+// c::set('panel.install', true);
 
 /*
 
