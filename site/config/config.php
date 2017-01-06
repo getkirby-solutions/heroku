62ba802fb7ed6faca3923933351b0bdd78e2a3cd
heyallan <allanmoreno@live.com>
2017-01-06 14:41:24 -0600

    Update kirby config file

--- site/config/config.php
+++ site/config/config.php
@@ -1,32 +1,11 @@
 <?php
 
-/*
-
----------------------------------------
-License Setup
----------------------------------------
-
-Please add your license key, which you've received
-via email after purchasing Kirby on http://getkirby.com/buy
-
-It is not permitted to run a public website without a
-valid license key. Please read the End User License Agreement
-for more information: http://getkirby.com/license
-
-*/
-
-c::set('license', 'put your license key here');
-c::set('debug', true);
+c::set('license','put your license key here');
+c::set('panel.info.license',false);
+c::set('timezone','America/Costa_Rica');
+c::set('locale','es_CR.UTF8');
 c::set('panel.install', true);
-
-/*
-
----------------------------------------
-Kirby Configuration
----------------------------------------
-
-By default you don't have to configure anything to
-make Kirby work. For more fine-grained configuration
-of the system, please check out http://getkirby.com/docs/advanced/options
-
-*/
+c::set('panel.language', 'es_419');
+c::set('panel.stylesheet','https://unpkg.com/kirby-panel-themes/dist/azure.min.css');
+c::set('url','/');
+c::set('debug',true);
